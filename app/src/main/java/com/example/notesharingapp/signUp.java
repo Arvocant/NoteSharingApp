dd30e72bc8ed6aa42273134884c72792d8822fde
Arvo cant <aro.vant@student.ap.be>
2021-01-03 14:18:39 +0100

    Button's Login + signup go somewhere

--- app/src/main/java/com/example/notesharingapp/signUp.java
+++ app/src/main/java/com/example/notesharingapp/signUp.java
@@ -2,13 +2,45 @@ package com.example.notesharingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class signUp extends AppCompatActivity {
+
+    Button login_signup_tran, login_btn;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        image = findViewById(R.id.logo_image);
+        login_signup_tran = findViewById(R.id.login_screen);
+        login_btn = findViewById(R.id.login_btn);
+
+        login_signup_tran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signUp.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signUp.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
